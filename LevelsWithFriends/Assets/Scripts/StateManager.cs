91a7d44b3dc01d48ab4bb960441ea2b787821220
Katarina Weis <kxw4589@g.rit.edu>
2017-02-01 11:30:14 -0500

    Added SceneManager object with state manager script (gamestates still in progress)

---
+++ LevelsWithFriends/Assets/Scripts/StateManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+    enum GameState
+    {
+        Build,
+        MainMenu,
+        Test,
+        Pause,
+        Play
+    }
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
